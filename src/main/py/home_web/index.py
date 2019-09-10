35e6ddd7f978d7ca31d1216eb6ed53750eee45d5
tbaecker @pi <tbaecker.spam@gmail.com>
2019-09-10 18:18:51 +0200

    Presentation: date format and table width.

--- src/main/py/home_web/index.py
+++ src/main/py/home_web/index.py
@@ -19,14 +19,14 @@ conn = sqlite3.connect(DB_NAME)
 curs = conn.cursor()
 curs.execute("SELECT names, unit FROM sensors;")
 sensors = curs.fetchall()
-sensor_list = "<table><tr><th>Capteur</th><th>valeur</th></tr>"
+sensor_list = "<table width=\"90%\"><tr><th>Capteur</th><th>valeur</th></tr>"
 for sensor in sensors:
     (sensor_name, unit) = sensor
     curs.execute("SELECT MAX(epochtimestamp), value FROM raw_measures_" + sensor_name + ";")
     last_date_and_value = curs.fetchall()
     (epochdate, value) = last_date_and_value[0]
-    datestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epochdate))
-    sensor_list = sensor_list + "<tr><td>" + sensor_name + "</td><td>" + str(value) + " " + unit + "</td><td>" + datestr + "</td><td><img src=\"graph.svg\" /></td></tr>"
+    datestr = time.strftime('%A %-d %b. - %H:%M<small>:%S</small>', time.localtime(epochdate))
+    sensor_list = sensor_list + "<tr><td>" + sensor_name + "</td><td>" + str(value) + " " + unit + "</td><td>" + datestr + "</td><td><img src=\"graph.svg\" style=\"width:80px;height:22px;\" /></td></tr>"
 sensor_list = sensor_list + "</table>"
 
 html = """<!DOCTYPE html>
