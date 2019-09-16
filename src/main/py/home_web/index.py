cb65bf07bb4a3300ea86098ecf448c082238b346
tbaecker @pi <tbaecker.spam@gmail.com>
2019-09-16 13:06:46 +0200

    minors

--- src/main/py/home_web/index.py
+++ src/main/py/home_web/index.py
@@ -21,14 +21,14 @@ conn = sqlite3.connect(DB_NAME)
 curs = conn.cursor()
 curs.execute("SELECT names, unit FROM sensors;")
 sensors = curs.fetchall()
-sensor_list = "<table width=\"90%\"><tr><th>Capteur</th><th>valeur</th></tr>"
+sensor_list = "<table style=\"border: .06em solid black;padding-left: 1em;padding-right: 2em;\"><tr><th style=\"padding-left: 1em;padding-right: 2em;\">Capteur</th><th style=\"padding-left: 1em;padding-right: 2em;\">valeur</th><th style=\"padding-left: 1em;padding-right: 2em;\">date</th><th style=\"padding-left: 1em;padding-right: 2em;\"></th></tr>"
 for sensor in sensors:
     (sensor_name, unit) = sensor
     curs.execute("SELECT MAX(epochtimestamp), value FROM raw_measures_" + sensor_name + ";")
     last_date_and_value = curs.fetchall()
     (epochdate, value) = last_date_and_value[0]
     datestr = time.strftime('%A %-d %b. - %H:%M<small>:%S</small>', time.localtime(epochdate))
-    sensor_list = sensor_list + "<tr><td>" + sensor_name + "</td><td>" + str(value) + " " + unit + "</td><td>" + datestr + "</td><td><img src=\"graph.svg\" style=\"width:80px;height:22px;\" /></td></tr>"
+    sensor_list = sensor_list + "<tr><td style=\"padding-left: 1em;padding-right: 2em;\">" + sensor_name + "</td><td style=\"padding-left: 1em;padding-right: 2em;\">" + str(value) + " " + unit + "</td><td style=\"padding-left: 1em;padding-right: 2em;\">" + datestr + "</td><td style=\"padding-left: 1em;padding-right: 2em;\"><img src=\"graph.svg\" style=\"width:5em;height:2em;\" /></td></tr>"
 sensor_list = sensor_list + "</table>"
 
 html = """<!DOCTYPE html>
