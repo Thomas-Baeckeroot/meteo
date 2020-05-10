ab47782bdf217b3693e99ece80a680fd887cd08a
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-05-10 23:08:00 +0200

    Start Python3 compatibility/migration.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 printf '\n\n*** Install PIP (package manager)... ***\n'
-sudo apt install -y python-pip
+sudo apt install -y python-pip python3-pip
 # Former below method is not recommended anymore
 # curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 # python /tmp/get-pip.py
@@ -12,7 +12,9 @@ printf '\n\n*** Install Picamera Python module... ***\n'
 sudo apt install -y python-picamera python3-picamera
 
 printf '\n\n*** Install postgresql database... ***\n'
-sudo apt install -y postgresql libpq-dev python-psycopg2 && pip install psycopg2
+sudo apt install -y postgresql libpq-dev python-psycopg2
+pip install psycopg2
+pip3 install psycopg2
 
 # check https://raspberrypi.stackexchange.com/questions/70018/remotely-debug-python-code-on-pi-using-eclipse-in-windows for further details
 printf '\n\n*** Install pydevd for Python remote debugging... ***\n'
@@ -20,6 +22,7 @@ sudo pip install pydevd
 
 printf '\n\n*** Install gpiozero for CPU temperature reading and other GPIO... ***\n'
 pip install gpiozero
+pip3 install gpiozero
 
 # A tutorial? about how to use the pressure/humidity/light/temperature sensors with I2C/SPI:
 # https://learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial/all
@@ -27,12 +30,19 @@ pip install gpiozero
 # https://pypi.org/project/tsl2561/
 printf '\n\n*** Install RPi.GPIO for input/output management... ***\n'
 pip install RPi.GPIO
+pip3 install RPi.GPIO
 
 printf '\n\n*** Install Adafruit_GPIO for input/output management... ***'
 python -m pip install Adafruit_GPIO  # Was not working without calling by 'python -m'
+pip3 install Adafruit_GPIO  # Was not working without calling by 'python -m'
 
 printf '\n\n*** Install tsl2561 for sensors reading... ***\n'
 pip install tsl2561
+pip3 install tsl2561
+
+printf '\n\n*** Install bluetin.io for HC-SR04 sensor reading... ***\n'
+pip install Bluetin_Echo
+pip3 install Bluetin_Echo
 
 # As detailed https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
 printf '\n\n*** Install gpac to get 'MP4Box' command for webcam video captures... ***\n'
@@ -51,6 +61,7 @@ cd /tmp
 git clone https://github.com/adafruit/Adafruit_Python_BMP.git
 cd Adafruit_Python_BMP
 sudo python setup.py install
+sudo python3 setup.py install
 
 printf '\n\n*** Create folder where images will be saved... ***\n'
 mkdir /home/pi/meteo/captures/
