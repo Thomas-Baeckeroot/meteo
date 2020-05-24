02b1c4de496fb56f5721b6556b213a950764bbdb
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-05-24 16:53:22 +0200

    Add local timestamp.

--- src/main/py/utils.py
+++ src/main/py/utils.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import calendar
 import datetime
 import time
 
 
 def epoch_now():
-    return calendar.timegm(time.gmtime())
+    # return calendar.timegm(time.gmtime())
+    return int(time.time())
 
 
-def iso_timestamp_now():
-    return datetime.datetime.utcfromtimestamp(epoch_now()).isoformat()
+def iso_timestamp_now():  # example: 2020-05-03T15:49:48
+    return iso_timestamp(epoch_now())
 
 
 def iso_timestamp(epoch):
-    return datetime.datetime.utcfromtimestamp(epoch).isoformat()
+    return datetime.datetime.utcfromtimestamp(epoch).isoformat(sep='Z')
 
 
 def iso_timestamp4files():
     return iso_timestamp_now().replace(':', '-')
+
+
+def local_timestamp_now():  # example: 2020-05-03T15:49:48
+    return local_timestamp(epoch_now())
+
+
+def local_timestamp(epoch):
+    if time.localtime().tm_isdst and time.daylight:
+        separator = 'S'  # Summer time
+    else:
+        separator = 'W'  # Winter time
+    return datetime.datetime.fromtimestamp(epoch).isoformat(sep=separator)
