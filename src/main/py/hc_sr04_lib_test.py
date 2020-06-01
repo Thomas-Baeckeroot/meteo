6a55ad76aeb905eb55b74508e67f4e9a30866642
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-06-01 19:31:21 +0200

    Add distance measurements to PostgreSQL DB.

--- src/main/py/hc_sr04_lib_test.py
+++ src/main/py/hc_sr04_lib_test.py
@@ -4,17 +4,20 @@
 import os
 import sys
 import Bluetin_Echo
+import psycopg2  # ProgreSQL
+import time
 
-from utils import local_timestamp_now
+import utils
 
 TRIGGER_PIN = 22
 ECHO_PIN = 23
 MAX_DISTANCE = 8  # Get really noisy over 3~4 meters
 UNIT = u'm'
 
+
 def measure_distance(temp_celcius=20):
     # Initialise Sensor with pins, speed of sound.
-    speed_of_sound = 331.5 + ( 0.6 * temp_celcius )
+    speed_of_sound = 331.5 + (0.6 * temp_celcius)
     echo = Bluetin_Echo.Echo(TRIGGER_PIN, ECHO_PIN, speed_of_sound)
     # echo.default_unit(UNIT)  # causes 'TypeError: 'str' object is not callable'
     echo.max_distance(value=MAX_DISTANCE, unit=UNIT)
@@ -22,7 +25,7 @@ def measure_distance(temp_celcius=20):
     # Measure Distance n times, return average.
     samples = 20
     result = echo.read(UNIT, samples)
-    #result = echo.read()
+    # result = echo.read()
     # Reset GPIO Pins.
     echo.stop()
     return result
@@ -34,7 +37,7 @@ if __name__ == "__main__":
         n_loop = int( sys.argv[1] )
     else:
         n_loop = 1
-    print(local_timestamp_now() + " - " + str(n_loop) + " loops to be performed...")
+    print(utils.local_timestamp_now() + " - " + str(n_loop) + " loops to be performed...")
     
     d_min = 99999
     d_max = -1
@@ -53,9 +56,25 @@ if __name__ == "__main__":
 
         # Print result.
         if n_measures!=0:
-            print(local_timestamp_now() + " - %.3f -> min-avg-max\t%.3f\t%.3f\t%.3f" % (d, d_min, d_total/n_measures, d_max))
+            print(utils.local_timestamp_now() + " - %.3f -> min-avg-max\t%.3f\t%.3f\t%.3f" % (d, d_min, d_total/n_measures, d_max))
+
+            try:
+                connection = psycopg2.connect(database="meteo")
+                cursor = connection.cursor()
+                insert_query = "INSERT INTO raw_measures VALUES(" + str(utils.epoch_now()) + "," + str(d) + ",'WaterRes');"
+                cursor.execute(insert_query)
+                connection.commit()
+            except (Exception, psycopg2.DatabaseError) as error :
+                print ("Error while creating PostgreSQL table", error)
+            finally:
+                # closing database connection.
+                if (connection):
+                    cursor.close()
+                    connection.close()
+                    print("PostgreSQL connection is closed")
+
             sys.stdout.flush()
 
     sys.stdout.flush()
-    os.system("sudo /sbin/shutdown -P +1 &")
-
+    # time.sleep(1)
+    # os.system("sudo /sbin/shutdown -P now &")
