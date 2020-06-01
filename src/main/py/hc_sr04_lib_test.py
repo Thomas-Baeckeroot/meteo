f2d6d08bea94261fa205e9887af7150408958d42
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-06-02 00:35:52 +0200

    Store rounded values in DB.

--- src/main/py/hc_sr04_lib_test.py
+++ src/main/py/hc_sr04_lib_test.py
@@ -7,12 +7,13 @@ import Bluetin_Echo
 import psycopg2  # ProgreSQL
 import time
 
+import sensors_functions
 import utils
 
 TRIGGER_PIN = 22
 ECHO_PIN = 23
 MAX_DISTANCE = 8  # Get really noisy over 3~4 meters
-UNIT = u'm'
+UNIT = u'cm'
 
 
 def measure_distance(temp_celcius=20):
@@ -34,17 +35,24 @@ def measure_distance(temp_celcius=20):
 def insert_raw_measures(timestamp, measure, sensor_short_name):
     try:
         connection = psycopg2.connect(database="meteo")
-        cursor = connection.cursor()
+        curs = connection.cursor()
+        read_decimals_query = "SELECT decimals FROM SENSORS WHERE names = '" + sensor_short_name + "';"
+        curs.execute(read_decimals_query)
+        decimals = int(curs.fetchone()[0])
         insert_query = "INSERT INTO raw_measures VALUES("\
-                       + str(timestamp) + "," + str(measure) + ", '" + sensor_short_name + "');"
-        cursor.execute(insert_query)
+                       + str(timestamp) + "," \
+                       + str(sensors_functions.round_value_decimals(measure, decimals)) + ", '" \
+                       + sensor_short_name + "');"
+        # + str(measure) + ", '"
+        curs.execute(insert_query)
         connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
         print("Error while creating PostgreSQL table", error)
     finally:
         # closing database connection.
         if (connection):
-            cursor.close()
+            curs.close()
             connection.close()
             print("PostgreSQL connection is closed")
 
@@ -57,14 +65,14 @@ if __name__ == "__main__":
         n_loop = 1
     print(utils.local_timestamp_now() + " - " + str(n_loop) + " loops to be performed...")
 
-    d_min = 99999
-    d_max = -1
-    d_total = 0
+    d_min = 99999.9
+    d_max = -1.0
+    d_total = 0.0
     n_measures = 0
 
     for i in range(n_loop):
         d = measure_distance()
-        if d != 0 and d <= MAX_DISTANCE:  # ignore measures out of interval [0 MAX_DISTANCE]
+        if d != 0. and d <= MAX_DISTANCE:  # ignore measures out of interval [0 MAX_DISTANCE]
             n_measures = n_measures + 1
             if d < d_min:
                 d_min = d
