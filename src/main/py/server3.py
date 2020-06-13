841a1f11f724a0d259fdc16b7c61b32b99f4f239
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-06-13 14:44:47 +0200

    Update graph.svg as live-coded.

--- src/main/py/server3.py
+++ src/main/py/server3.py
@@ -4,13 +4,44 @@
 
 import http.server
 import cgitb
-import logging
+# import logging
+import sys
 
-print = logging.info
+# Due to Logger unable to get error message details, then it has been commented.
+# Outputs will tentatively be catch by redirection on call.
 
-logging.basicConfig(filename='/home/web/server3.log', level=logging.DEBUG)
-logging.info('log started - INFO level')
-logging.error('log started - ERROR level')
+
+class LoggerWriter:
+    def __init__(self, level):
+        # self.level is really like using log.debug(message)
+        # at least in my case
+        self.level = level
+                                    
+    def write(self, message):
+        # if statement reduces the amount of newlines that are
+        # printed to the logger
+        if message != '\n':
+            self.level(message)
+                                                                                    
+    def flush(self):
+        # create a flush method so things can be flushed when
+        # the system wants to. Not sure if simply 'printing'
+        # sys.stderr is the correct way to do it, but it seemed
+        # to work properly for me.
+        self.level(sys.stderr)
+        
+
+# print = logging.info
+
+# logging.basicConfig(filename='/home/web/server3.log',level=logging.DEBUG)
+
+# log = logging.getLogger()
+# sys.stdout = LoggerWriter(log.info)
+# sys.stderr = LoggerWriter(log.warning)
+
+# logging.info('log started - INFO level')
+# logging.error('log started - ERROR level')
+
 cgitb.enable()
 
 PORT = 49107
