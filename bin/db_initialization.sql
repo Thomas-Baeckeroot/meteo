6135ac68c06469b8324ae556b75915e2eabab6b4
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2020-06-13 15:06:24 +0200

    Auto-format, clean-up and typos.

--- bin/db_initialization.sql
+++ bin/db_initialization.sql
@@ -33,10 +33,10 @@ CREATE TABLE consolidated_measures
     );
 
 -- INSERT INTO sensors VALUES('SensShortName' , p0_100, 'Sensor verbose texte'    , num_dec_0_2, false, 'Unit_cm', '900'); -- later, consolidated should be like '900 86400'
-INSERT INTO sensors VALUES('CPUtemp0', 10, 'Température CPU (centrale)' , 1, false, '°C' , '900', 'ignored');
-INSERT INTO sensors VALUES('CPUtemp1',  9, 'Température CPU (R0 grange)', 1, false, '°C' , '900', 'CPU_temp');
+INSERT INTO sensors VALUES('CPU_temp', 10, 'Température CPU (centrale)' , 1, false, '°C' , '900', 'ignored');
+INSERT INTO sensors VALUES('CPU_tmp1',  9, 'Température CPU (R0 grange)', 1, false, '°C' , '900', 'CPU_temp');
 INSERT INTO sensors VALUES('WaterRes', 50, 'Réserve eau de pluie'       , 1, false, 'cm' , '900', 'water_height');
-INSERT INTO sensors VALUES('lumin'   , 70, 'Luminosité'                 , 0, false, 'lux', '900', 'ignored');
+INSERT INTO sensors VALUES('lum_ext' , 70, 'Luminosité'                 , 0, false, 'lux', '900', 'ignored');
 INSERT INTO sensors VALUES('temp_ext', 90, 'Température'                , 1, false, '°C' , '900', 'ignored');
 INSERT INTO sensors VALUES('pressure', 80, 'Pression atmosphérique'     , 1, false, 'hPa', '900', 'ignored');
 
