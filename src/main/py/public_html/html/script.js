5abafad6762a76b3114c88b41b69b0f876428299
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2023-11-18 21:04:35 +0100

    Change method to build the top Picture Selector.

--- src/main/py/public_html/html/script.js
+++ src/main/py/public_html/html/script.js
@@ -70,66 +70,132 @@ function updateCurrentImage(pictureData) {
     document.getElementById("error_message").textContent = metadata.error_message; // TODO Implement error message display
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
-    console.log("DOMContentLoaded - ---DOMContentLoaded-start---");
-
-    // Appeler la fonction fetchData pour obtenir les données au chargement de la page
-    console.log("DOMContentLoaded - before fetchData call");
-    await fetchData(); // Wait for fetchData to complete
-    console.log("DOMContentLoaded - after fetchData call:", picturesData, metadata);
-    const picturesFolder = "captures/" + metadata.sensor + "/"
-    console.log("DOMContentLoaded - picturesFolder =", picturesFolder);
-
+function grayPictureSelectorWithoutEvent() {
     for (let hour = 0; hour <= 23; hour++) {
         for (let minute = 0; minute < 60; minute += 15) {
             // Format the hour and minute as "HHhMM"
             const formattedHour = hour.toString().padStart(2, '0');
             const formattedMinute = minute.toString().padStart(2, '0');
             // Create the time string
-            const identif = `${formattedHour}h${formattedMinute}`;
-            // Now, you can use the 'identif' variable in your loop
-            const picturedata = picturesData[identif];
+            const hh_mm = `${formattedHour}h${formattedMinute}`;
+            console.log("Checking if element '" + hh_mm + "' has listeners...");
+            // Assuming hh_mm_element is your HTML element
+            const hh_mm_element = document.getElementById(hh_mm);
+            // Get the event listeners for the "click" event
 
-            // Get the element by its ID
-            const element = document.getElementById(identif);
-            if (picturedata) {
-                // Update the displayed current_image
-                console.log("DOMContentLoaded - addEventListener: '" + identif + "' -> '" + picturedata + "'")
-                element.addEventListener("click", function () {
-                    updateCurrentImage(picturedata);
-                });
-                // Add an event listener to each table cell
+            const text = hh_mm_element.textContent
+            console.log("content is '" + text + "'")
 
-                if (picturedata.fSize < SIZE_LIMIT) {
-                    //element.style.backgroundColor = rgba(173, 216, 230, 0.5);
-                    element.style.backgroundColor = "LightBlue";
-                } else {
-                    element.style.backgroundColor = "Khaki";
-                }
-                element.style.cursor = "pointer";
-
-                // Add a mouseover event listener
-                element.addEventListener("mouseover", function () {
-                    element.classList.add("hovered-cell");
-                });
-                // Add a mouseout event listener
-                element.addEventListener("mouseout", function () {
-                    element.classList.remove("hovered-cell");
-                });
-
-                /*
-                * cell:hover {
-background-color: lightblue
-                * */
-
+            const clickListeners = hh_mm_element.onclick;
+            // Check if there are click event listeners
+            if (text === "-") {
+                console.log("Element '" + hh_mm + "' has a single 'dash' as text content.");
+                document.getElementById(hh_mm).style.backgroundColor = "DarkGray";
+                document.getElementById(hh_mm).style.cursor = "default";
             } else {
-                console.log("DOMContentLoaded - No 'picturedata' for identif '" + identif + "'.");
-                document.getElementById(identif).style.backgroundColor = "DarkGray";
-                document.getElementById(identif).style.cursor = "default";
-                /* add "cursor: pointer;" */
+                console.log("Element '" + hh_mm + "' has text content = '" + text + "'.");
             }
         }
     }
+}
+
+function updatePictureSelector(hh_mm, pictureData) {
+    const mm = hh_mm.slice(-2)
+    const mmAsInt = parseInt(mm)
+    let hh_mm15
+    if (mmAsInt % 15 === 0) {
+        // mmAsInt is a multiple of 15
+        hh_mm15 = hh_mm
+    } else {
+        // mmAsInt is not a multiple of 15
+        hh_mm15 = hh_mm.substring(0, 3) + (mmAsInt - (mmAsInt % 15)).toString()
+    }
+
+    // Get the hh_mm_element by its ID
+    const hh_mm_element = document.getElementById(hh_mm15);
+
+    hh_mm_element.textContent = mm
+
+    // Update the displayed current_image
+    console.log("DOMContentLoaded - addEventListener: '" + hh_mm + "' -> '" + pictureData + "'")
+    hh_mm_element.addEventListener("click", function () {
+        updateCurrentImage(pictureData);
+    });
+    // Add an event listener to each table cell
+
+    if (pictureData.fSize < SIZE_LIMIT) {
+        //hh_mm_element.style.backgroundColor = rgba(173, 216, 230, 0.5);
+        hh_mm_element.style.backgroundColor = "LightBlue";
+    } else {
+        hh_mm_element.style.backgroundColor = "Khaki";
+    }
+    hh_mm_element.style.cursor = "pointer";
+
+    // Add a mouseover event listener
+    hh_mm_element.addEventListener("mouseover", function () {
+        hh_mm_element.classList.add("hovered-cell");
+    });
+    // Add a mouseout event listener
+    hh_mm_element.addEventListener("mouseout", function () {
+        hh_mm_element.classList.remove("hovered-cell");
+    });
+
+    /*
+    * cell:hover {
+background-color: lightblue
+    * */
+}
+
+function fillPictureSelector_deprecated() {
+    for (let hour = 0; hour <= 23; hour++) {
+        for (let minute = 0; minute < 60; minute += 15) {
+            // Format the hour and minute as "HHhMM"
+            const formattedHour = hour.toString().padStart(2, '0');
+            const formattedMinute = minute.toString().padStart(2, '0');
+            // Create the time string
+            const hh_mm = `${formattedHour}h${formattedMinute}`;
+            // Now, you can use the 'hh_mm' variable in your loop
+            const pictureData = picturesData[hh_mm];
+
+            if (pictureData) {
+                updatePictureSelector(hh_mm, pictureData)
+            } else {
+                console.log("DOMContentLoaded - No 'pictureData' for hh_mm '" + hh_mm + "'.");
+                document.getElementById(hh_mm).style.backgroundColor = "DarkGray";
+                document.getElementById(hh_mm).style.cursor = "default";
+            }
+        }
+    }
+}
+
+function fillPictureSelector() {
+    // Iterate through each element inside "pictures"
+    for (const element_hh_mm in picturesData) { // rely on
+        if (picturesData.hasOwnProperty(element_hh_mm)) {
+            let pictureData = {};
+            pictureData.img = undefined;
+            pictureData.fSize = undefined;
+            pictureData = picturesData[element_hh_mm];
+            console.log(`Element ${element_hh_mm}:`, pictureData);
+            // Your logic for each element goes here
+
+            updatePictureSelector(element_hh_mm, pictureData)
+        }
+    }
+
+    grayPictureSelectorWithoutEvent()
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    console.log("DOMContentLoaded - ---DOMContentLoaded-start---");
+
+    // Call fetchData function to get data after page loaded:
+    await fetchData(); // Wait for fetchData to complete
+    console.log("DOMContentLoaded - after fetchData call:", picturesData, metadata);
+    const picturesFolder = "captures/" + metadata.sensor + "/"
+    console.log("DOMContentLoaded - picturesFolder =", picturesFolder);
+
+    fillPictureSelector()
 
     console.log("DOMContentLoaded - ---DOMContentLoaded-end---");
 });
