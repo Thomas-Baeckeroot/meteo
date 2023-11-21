2290ac80de6ec85515085cd52dd90d219801151f
Thomas Baeckeroot <tbaeckeroot@sophiagenetics.com>
2023-11-21 21:59:03 +0100

    Update DB backup script (to run manually).

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Purpose = Backup of meteo DB and scripts
+DBNAME="meteo"
+DBUSER="meteo"
+DBPASSWORD="SetRandomPassword123"
 DATE=$(date +"%Y-%m-%d")
-# TODO Backup of PostgreSQL DB to dump file
-tar -zcvf "$HOME/meteo_$DATE.tgz" "$HOME/meteo"
+# Option --show-progress-size not available yet on mysqldump v10.19
+# Option --password="${DBPASSWORD}" not available on mysqldump v10.19
+nice -n 19 mysqldump --user="${DBUSER}" --password="${DBPASSWORD}" --databases "${DBNAME}" > "/volume1/homes/meteo/mysqldump_${DBNAME}_${DATE}.sql"
+gzip "/volume1/homes/meteo/mysqldump_${DBNAME}_${DATE}.sql"
